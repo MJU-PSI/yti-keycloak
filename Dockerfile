f8de77ff93f63de310e2fe4fa6dcd1e717314036
Tadej Šivak <tadej.sivak@gora.si>
2023-09-12 22:28:37 +0200

    changed logging format and TZ

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM jboss/keycloak
 
+ENV TZ=Europe/Ljubljana
+
 COPY entrypoint.sh /
+RUN chmod +x /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,8 @@ else
   HTTP_RELATIVE_PATH="${KEYCLOAK_HTTP_RELATIVE_PATH}"/
 fi
 
+sed -i -e 's#<pattern-formatter pattern=\".*\"\/>#<pattern-formatter pattern=\"%d{dd.MM.yyyy HH:mm:ss.SSS} %-5p (%13F:%L) %3x - %m%n\"\/>#' /opt/jboss/keycloak/standalone/configuration/standalone.xml
+sed -i -e 's#<pattern-formatter pattern=\".*\"\/>#<pattern-formatter pattern=\"%d{dd.MM.yyyy HH:mm:ss.SSS} %-5p (%13F:%L) %3x - %m%n\"\/>#' /opt/jboss/keycloak/standalone/configuration/standalone-ha.xml
 sed -i -e 's#<web-context>auth<\/web-context>#<web-context>'"$HTTP_RELATIVE_PATH"'auth<\/web-context>#' /opt/jboss/keycloak/standalone/configuration/standalone.xml
 sed -i -e 's#<web-context>auth<\/web-context>#<web-context>'"$HTTP_RELATIVE_PATH"'auth<\/web-context>#' /opt/jboss/keycloak/standalone/configuration/standalone-ha.xml
 sed -i -e 's#\/auth#\/'"$HTTP_RELATIVE_PATH"'auth#' /opt/jboss/keycloak/welcome-content/index.html 
